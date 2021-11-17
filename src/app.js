56921c871f49c38068be2514a83379f7444be67c
Felipe Mestre <42158804+FelipeMestre@users.noreply.github.com>
2021-11-17 23:27:21 +0000

    Post method for the form

--- src/app.js
+++ src/app.js
@@ -14,10 +14,6 @@ app.use(express.urlencoded({
   extended: true
 }));
 
-app.post("/*", jsonParser, function(req, res) {
-  res.status(404).send("404 Error not found");
-});
-
 var experiencia = {
   "experiencia-laboral": [
     {
@@ -32,19 +28,34 @@ var experiencia = {
       "position": "Full Stack Developer",
       "description": "Mi rol consistio en ser desarrollador junior haciendo frontend con angular, backend con ASP.net y con dos bases de datos en sql server. El proyecto fue un sistema de gestion de viajes entre cosechas de madera, acopios intermedios y Montevideo. \n Desarrolle funcionalidades a lo largo de sprints de dos semanas durante cuatro meses. Entre estas funcionalidades estan un sistema de notificaciones, la integracion este este sistema con otro mediante requests http y un modulo de gestion documentacion que verifica la correctitud de la misma para los viajes a realizarse.",
       "initialDate": new Date("2021/06/21"),
-      "finishedDate": null
+      "finishedDate": new Date("2022/06/21")
     }
   ]
 };
 
 var jsonParser = bodyParser.json();
 
-app.get('/experiencia', function(req, res) {
+app.get('/experiencia-laboral', function(req, res) {
   res.send(experiencia);
 });
 
-app.listen(process.env.PORT || 3000, (a) => {
-  console.log("Servidor disponible en http://localhost:3000")
+app.listen(process.env.PORT || 4000, (a) => {
+  console.log("Servidor disponible en http://localhost:4000")
 });
- 
+
+app.post('/enviar-formulario', jsonParser, function(req, res) {
+  const nombreContacto = req.body.nombreContacto;
+  if (!nombreContacto) {
+    return res.status(400).send("Falta el nombre de contacto");
+  }
+  res.cookie("PW_2021-CV_Contacto", nombreContacto);
+  res.send("Se completó la operación con éxito");
+});
+
+app.post("/*", jsonParser, function(req, res) {
+  res.status(404).send("404 Error not found");
+});
+
+
+
 module.exports = app;
